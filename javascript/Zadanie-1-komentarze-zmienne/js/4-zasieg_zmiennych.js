2f873e6111d8b2e3e007a967576fba6db04bf07e
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-06-01 12:19:22 +0200

    Przerobiliśmy zasięg danych i typy danych

---
+++ javascript/Zadanie-1-komentarze-zmienne/js/4-zasieg_zmiennych.js
@@ -0,0 +1,21 @@
+//przestrzeń globalna
+
+//zmienne stworzone w tym zasięgu "widoczne" są wszędzie
+
+let number = 426;
+console.log(number);
+
+const addNumber = () => {
+    /*
+    wchodząc do bloku kodu, czyli w przestrzeń między { a }
+    wchodzimy w zasięg lokalny.
+    */
+    
+console.log(number);
+let localNumber = 1026;
+console.log(localNumber);
+}
+
+//wywołanie funkcji
+addNumber();
+console.log(localNumber); // wyświetli nam tu błąd
