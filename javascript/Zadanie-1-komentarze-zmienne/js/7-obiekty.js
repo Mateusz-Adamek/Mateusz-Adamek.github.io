51261285c32086cd94c968d975240085e1ded7f3
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-06-01 14:56:16 +0200

    Zdefiniowanie nowej klasy i stworzenie obiektu na jej bazie

---
+++ javascript/Zadanie-1-komentarze-zmienne/js/7-obiekty.js
@@ -0,0 +1,53 @@
+/* let samochod = {
+    marka: 'Ferrari', 
+    moc: 415,
+    kolor: 'czerwony',
+    przedstawSie() {
+        return `Nazywam się ${this.marka} i mam moc ${this.moc} KM`;
+    },
+    odpal () {
+        console.log('Brum, brum');
+    }
+}
+
+const instancjaSamochodu1 = samochod.przedstawSie(); //wywołanie
+console.log(instancjaSamochodu1);
+
+samochod.marka = 'Fiat';
+samochod.moc = 80;
+const instancjaSamochodu2 = samochod.przedstawSie();
+console.log(instancjaSamochodu2);
+
+samochod.odpal(); //wywolanie */
+
+
+
+
+class Ogloszenie {
+    constructor(id, tytul, tresc, cena, lokalizacja) {
+        this.id = id;
+        this.tytul = tytul; //prawa strona ma odpowiadać parametrowi w konstruktorze
+        this.tresc = tresc; //lewa strona to jest nasz wybór jak my to sobie nazwiemy
+        this.cena = cena;
+        this.lokalizacja = lokalizacja;
+    }
+
+    wyswietl() {
+        document.write(
+            `Ogłoszenie nr ${this.id}<br>
+            Tytuł: ${this.tytul} <br>
+            Treść: ${this.tresc}<br>
+            Cena: ${this.cena}<br>
+            Lokalizacja: ${this.lokalizacja}
+            `)
+    }
+}
+
+const ogloszenie1 = new Ogloszenie(  //new tworzy nowy obiekt, na bazie podanej klasy
+    000122345,
+    'Sprzedam multiplę',
+    'Sprawna, tylko na msze jeżdżona',
+    12000,
+    'Warszawa'
+)
+ogloszenie1.wyswietl();
