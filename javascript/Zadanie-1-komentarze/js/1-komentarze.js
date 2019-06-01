552a1c212728542778746081f327e908af56c1a3
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-06-01 09:41:30 +0200

    Ogarnięte komentarze i warningi

---
+++ javascript/Zadanie-1-komentarze/js/1-komentarze.js
@@ -0,0 +1,16 @@
+//Komentarz przykładowy - liniowy
+
+
+
+console.log('Test czytania pliku');
+console.error('Błąd czytania pliku');
+console.warn('Ostrzeżenie');
+
+
+
+/*
+To jest komentarz blokowy
+Można pisać w iluś liniach
+i niczego nie będzie wyświetlał
+
+*/
