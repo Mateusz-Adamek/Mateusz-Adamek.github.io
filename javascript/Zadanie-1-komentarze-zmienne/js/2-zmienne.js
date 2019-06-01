a3fcc1aa407692b8e1f54de25bdda32c1076e6ed
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-06-01 10:22:42 +0200

    Zaczynamy funkcje

--- javascript/Zadanie-1-komentarze-zmienne/js/2-zmienne.js
+++ javascript/Zadanie-1-komentarze-zmienne/js/2-zmienne.js
@@ -7,7 +7,19 @@ const uiVersion =2;
 document.write('app/ui/'+uiVersion+'/step-1'); 
 
 
+// Kolejna część użycia zmiennych
+let imie = 'Mateusz';
+let nazwisko = 'Adamek';
+let wiek = 26;
 
+console.log(imie, nazwisko, wiek);
+
+//Nadpisywanie zmiennych
+imie= 'Ukasz';
+nazwisko= 'Jaje';
+wiek = 25;
+
+console.log(imie, nazwisko, wiek);
 
 
 
