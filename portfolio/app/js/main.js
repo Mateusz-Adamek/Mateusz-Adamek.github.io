d197420b319839858f35d4ad22271d65d1395be0
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-07-27 16:19:26 +0200

    Poprawiono uklad blokow w sekcji Examples, dodatkowo wprowadzono validacje js

---
+++ portfolio/app/js/main.js
@@ -0,0 +1,27 @@
+const name = document.getElementById('name');
+const mail = document.getElementById('mail');
+const message = document.getElementById('message');
+// const agreementOne = document.getElementById('zgoda-marketingowa-1');
+// const agreementTwo = document.getElementById('zgoda-marketingowa-2');
+// const agreementAll = document.getElementById('wszystkie-zgody');
+const submit = document.getElementById('submit-message');
+
+const validation = (e) => {
+    const text1 = name.value;
+    const text2 = mail.value;
+    const text3 = message.value;
+    // let check1 = agreementOne.checked;
+    // let check2 = agreementTwo.checked;
+    // const check3 = agreementAll.checked;
+
+    if (text1 == null || text2 == null ||  text3 == null ){ 
+        setCustomValidity("Please enter any text into the input")
+    // } else if (check3.checked) {
+    //     let check1 = ;
+    //     let check2 = ;
+    } else {
+
+    }
+}
+
+submit.addEventListener('click', validation);
