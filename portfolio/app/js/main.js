8496e49c6898d22998a92e9be4b60bd6dd341483
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-07-27 17:56:06 +0200

    Poprawiono błąd z nazewnictwem klas w js

--- portfolio/app/js/main.js
+++ portfolio/app/js/main.js
@@ -1,58 +1,57 @@
-$ (function() {
-// const name = document.getElementById('name');
-// const mail = document.getElementById('mail');
-// const message = document.getElementById('message');
-// const agreementOne = document.getElementById('zgoda-marketingowa-1');
-// const submit = document.getElementById('submit-message');
+$(function () {
+    // const name = document.getElementById('name');
+    // const mail = document.getElementById('mail');
+    // const message = document.getElementById('message');
+    // const agreementOne = document.getElementById('zgoda-marketingowa-1');
+    // const submit = document.getElementById('submit-message');
 
-// const validation = (e) => {
-//     const text1 = name.value;
-//     const text2 = mail.value;
-//     const text3 = message.value;
-//     // let check1 = agreementOne.checked;
-
-
-//     if (text1 == null || text2 == null ||  text3 == null ){ 
-//         setCustomValidity("Please enter any text into the input");
-//     // } else if (check3.checked) {
-//     //     let check1 = ;
-//     //     let check2 = ;
-//     } else {
-
-//     }
-// }
-
-// submit.addEventListener('click', validation);
+    // const validation = (e) => {
+    //     const text1 = name.value;
+    //     const text2 = mail.value;
+    //     const text3 = message.value;
+    //     // let check1 = agreementOne.checked;
 
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "block") {
-//       x.style.display = "none";
-//     } else {
-//       x.style.display = "block";
-//     }
-//   }
+    //     if (text1 == null || text2 == null ||  text3 == null ){ 
+    //         setCustomValidity("Please enter any text into the input");
+    //     // } else if (check3.checked) {
+    //     //     let check1 = ;
+    //     //     let check2 = ;
+    //     } else {
+
+    //     }
+    // }
+
+    // submit.addEventListener('click', validation);
 
 
-  $(".mobile-menu").on({
-    click: function(){
+    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+    // function myFunction() {
+    //     var x = document.getElementById("myLinks");
+    //     if (x.style.display === "block") {
+    //       x.style.display = "none";
+    //     } else {
+    //       x.style.display = "block";
+    //     }
+    //   }
 
 
-       if($(this).hasClass("active")){
+    $(".menu-mobile").on({
+        click: function () {
 
-          $(".mobile-menu").removeClass("active");
-          $("home nav").removeClass("open");
+            if ($(this).hasClass("active")) {
 
-       }else{
+                $(".menu-mobile").removeClass("active");
+                $("home nav").removeClass("open");
 
-          $(".mobile-menu").addClass("active");
-          $("home nav").addClass("open");
+            } else {
 
-       }
+                $(".menu-mobile").addClass("active");
+                $("home nav").addClass("open");
 
-    }
- });
+            }
+            console.log("click");
+        }
+    });
 
 });
