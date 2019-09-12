d7129aefa62a3f93430fd7fcacdc8b7864c85c39
Mateusz Adamek <mateusz.4damek@gmail.com>
2019-09-12 08:43:57 +0200

    Further tests

--- zadanie-testowe1/js/main.js
+++ zadanie-testowe1/js/main.js
@@ -1,22 +1,65 @@
-let params = {q: '#polska', count: 5};
+let query = 'from:polska filter:images';
  
-client.get('search/tweets', params, function(error, tweets, response) {
+let date = new Date();
+date.setDate(date.getDate() - 7);
+ 
+let date_query = date.getFullYear()+'-'+date.getMonth()+'-'+date.getDate();
+ 
+//let params = {q: query, count: 100, tweet_mode: 'extended', util: date_query};
+let params = {screen_name: 'polska', count: 100, tweet_mode: 'extended', util: date_query};
+ 
+let test = '';
+ 
+//client.get('search/tweets', params, function(error, tweets, response) {
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
         //console.log(tweets.statuses[0].entities.media);
         //console.log(tweets.statuses[0].entities.media);
  
  
-        tweets.statuses.forEach((element, index) => {
+        tweets.forEach((element, index) => {
  
-            console.log(element.entities.media);
+            if(typeof element.extended_entities !== "undefined"){
+ 
+                let entities = element.extended_entities;
+                let mediaall = entities.media;
+ 
+                if(typeof entities !== "undefined" && typeof mediaall !== "undefined"){
+ 
+                    mediaall.forEach((el, ind) => {
+ 
+                        if(el.type === 'photo'){
+ 
+                            console.log(el.media_url);
+                            // console.log(el);
+ 
+                            test += '<img src="'+el.media_url+'">';
+ 
+                        }
+ 
+                    });
+ 
+                }
+ 
+            }
  
         });
+ 
     }else{
  
-        console.log('dupa');
+        console.log(error);
  
     }
 });
+ 
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/html','Content-Length':test.length});
+    res.write(test);
+    res.end();
+ 
+}).listen(8000);
+ 
+setTimeout(() => {opn('http://localhost:8000') }, 1000);
 
 
 //a w petli element.entities.media zwraca ci tablice z obiektami mediow, czyli obrazki
